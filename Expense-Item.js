8759b174bffda4afb5e6d6c2c7680899421af7d7
Mohammed Sadiq Mulla <122668317+Mdsadiqmulla@users.noreply.github.com>
2023-04-14 23:38:56 +0530

    Add files via upload

---
+++ Expense-Item.js
@@ -0,0 +1,15 @@
+function ExpenseItem() {
+    return (
+    <div>
+        <h3>Expense Item</h3>
+        <br></br>
+        <div>
+            <div>Food Rs. 10</div>
+            <div>Petrol Rs. 100</div>
+            <div>Movies Rs. 200</div>
+        </div>
+    </div>
+    );
+}
+
+export default ExpenseItem;
