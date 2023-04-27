53cbb10d9e83c971fda90615213273b038797730
Sadiq <sadiq78692431@gmail.com>
2023-04-28 01:07:38 +0530

    update the code till 6th task

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import ExpenseItem from './ExpenseItem';
+import ExpenseItem from '../components/ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
 
