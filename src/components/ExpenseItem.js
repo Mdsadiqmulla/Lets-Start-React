5d2dea2dd009000a544cb639ae55099640ed3122
Sadiq <sadiq78692431@gmail.com>
2023-05-18 10:43:19 +0530

    updated the code using props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,7 @@
-import './ExpenseItem.css';
+import './ExpenseItem.css'
 
 function ExpenseItem(props) {
+    
     return (
         <div className='expense-item'>
             <div>{props.date.toISOString()}</div>
